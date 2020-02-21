6ec7d0d3009b4ada4990ae34aa78d24ea6206023
tux0112 <43138394+mrasw@users.noreply.github.com>
2020-02-21 15:45:14 +0700

    Update soal1.sh

--- soal1/soal1.sh
+++ soal1/soal1.sh
@@ -1,20 +1,9 @@
-A_Raw
+#A_Raw
 
 awk '{gsub(OFS=", ",""); gsub("[:\\ ]",""); gsub("[:\\,]"," ");  print $21}' Sample-Superstore.csv > praktikum1_1.csv
  
-A_finish.
+#A_finish.
 
 awk '{l[$13] += $NF}END {for(i in l) if(i!="Region") print i, l[i]}' lagi1_1.csv | sort -grk2 | tail
 
 
-B_Finish
-
- awk '/Central/{l[$11 $13] += $NF}END {for(i in l) if(i!="Region") print i, l[i]}' lagi1_1.csv | sort -gk2 | head -n2
-
-atau 
-
-awk '{l[$11" "$13] += $NF}END {for(i in l) if(i!="Region") print i, l[i]}' lagi1_1.csv | sort -gk3 | head -n2
-
-C_Finish
-
- awk '{l[$17" "$11" "$13] += $NF}END {for(i in l) if(i!="Region") print i, l[i]}' lagi1_1.csv | sort -grk4 | head -n10
